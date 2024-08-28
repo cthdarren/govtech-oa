67e25698d53588161351c831f532b75e1503acbc
cthdarren <ctze.darren@gmail.com>
2024-08-28 20:53:19 +0200

    pass all base test cases

--- backend/spec/routing/operations_routing_spec.rb
+++ backend/spec/routing/operations_routing_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe Api::V1::OperationsController, type: :routing do
   describe 'routing' do
     it 'routes to #add' do
-      expect(post: '/api/v1/add').to route_to('operation#add')
+      expect(post: '/api/v1/add').to route_to('api/v1/operations#add')
     end
 
     it 'routes to #subtract' do
-      expect(post: '/api/v1/subtract').to route_to('operation#subtract')
+      expect(post: '/api/v1/subtract').to route_to('api/v1/operations#subtract')
     end
   end
 end
