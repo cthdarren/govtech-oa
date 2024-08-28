4af54a9d098f41249c62e53b2477091bbb704965
cthdarren <ctze.darren@gmail.com>
2024-08-28 20:51:17 +0200

    base cases passed

--- backend/app/controllers/api/v1/operations_controller.rb
+++ backend/app/controllers/api/v1/operations_controller.rb
@@ -1,17 +1,25 @@
 class Api::V1::OperationsController < ApplicationController
+  before_action :assign_vars, :allowed_params
   # POST api/v1/add
   def add
-    render json: { status: 200, result: first + second }
+    first = params[:first]
+    second = params[:second]
+    render json: { status: 200, result: @first + @second }
   end
 
   # POST api/v1/subtract
   def subtract
-    render json: { status: 200, result: first - second }
+    render json: { status: 200, result: @first - @second }
   end
 
   private
 
-  def group_params
+  def allowed_params
     params.permit(:first, :second)
+  end
+
+  def assign_vars 
+    @first = params[:first].to_i
+    @second = params[:second].to_i
   end
 end
