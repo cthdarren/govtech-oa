4ae593fd4de1f70493679fcc66d8a4e0e1798726
cthdarren <ctze.darren@gmail.com>
2024-08-28 23:04:04 +0200

    completed functionality

--- frontend/src/OperationComponent.js
+++ frontend/src/OperationComponent.js
@@ -2,54 +2,82 @@ import React from "react";
 import { useState } from "react";
 
 function OperationComponent() {
-    const [first, setFirst] = useState();
-    const [second, setSecond] = useState();
+  const [first, setFirst] = useState();
+  const [second, setSecond] = useState();
+  const [result, setResult] = useState();
+  const [error, setError] = useState();
 
-    function handleFirstChange(e) {
-        setFirst(e.target.value);
-    }
+  function handleFirstChange(e) {
+    setFirst(e.target.value);
+  }
 
-    function handleSecondChange(e) {
-        setSecond(e.target.value);
-    }
+  function handleSecondChange(e) {
+    setSecond(e.target.value);
+  }
 
-    function submitAdd() {
-        fetch("http://localhost:3000/api/v1/add", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded",
-            },
-            body: new URLSearchParams({
-                first: first,
-                second: second,
-            }),
-        })
-            .then((response) => response.json())
-            .then((data) => console.log(data));
-    }
+  function submitAdd() {
+    // Referenced from: https://stackoverflow.com/questions/35325370/how-do-i-post-a-x-www-form-urlencoded-request-using-fetch
+    fetch("http://localhost:3000/api/v1/add", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams({
+        first: first,
+        second: second,
+      }),
+    }).then((response) => {
+      displayResult(response);
+    });
+  }
 
-    function submitSubtract() { }
+  function submitSubtract() {
+    // Referenced from: https://stackoverflow.com/questions/35325370/how-do-i-post-a-x-www-form-urlencoded-request-using-fetch
+    fetch("http://localhost:3000/api/v1/subtract", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams({
+        first: first,
+        second: second,
+      }),
+    }).then((response) => {
+      displayResult(response);
+    });
+  }
 
-    return (
-        <div>
-            <label>First number:</label>
-            <input
-                type="number"
-                onChange={(e) => handleFirstChange(e)}
-                value={first}
-            />
+  function displayResult(response) {
+    if (response.status === 200)
+      response.json().then((data) => setResult(data.result));
+    else if (response.status === 400)
+      response.json().then((data) => {
+        setResult();
+        setError(data.result);
+      });
+  }
 
-            <label>Second number:</label>
-            <input
-                type="number"
-                onChange={(e) => handleSecondChange(e)}
-                value={second}
-            />
+  return (
+    <div>
+      <label>First number:</label>
+      <input
+        type="number"
+        onChange={(e) => handleFirstChange(e)}
+        value={first}
+      />
 
-            <button onClick={submitAdd}> Add </button>
-            <button onClick={submitSubtract}> Subtract </button>
-        </div>
-    );
+      <label>Second number:</label>
+      <input
+        type="number"
+        onChange={(e) => handleSecondChange(e)}
+        value={second}
+      />
+
+      <button onClick={submitAdd}> Add </button>
+      <button onClick={submitSubtract}> Subtract </button>
+      <div>{result}</div>
+    </div>
+  );
 }
 
 export default OperationComponent;
