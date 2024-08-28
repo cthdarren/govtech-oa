52ba332b11aeb75b9cd5fe3b6e1b6d0150a39983
cthdarren <ctze.darren@gmail.com>
2024-08-29 01:47:16 +0200

    fixed all the production bugs, changed master key, removed from git

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -3,10 +3,12 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.hosts << /[a-z0-9]+\.darren-chan\.com/
+
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.enable_reloading = true
+  config.enable_reloading = false 
 
   # Do not eager load code on boot.
   config.eager_load = false
