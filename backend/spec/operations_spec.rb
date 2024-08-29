8e37cb1307a55c8cc20fc8c148fc07a32f1032f7
cthdarren <ctze.darren@gmail.com>
2024-08-29 17:35:05 +0200

    changed unit testing format to make it more reliable

--- backend/spec/operations_spec.rb
+++ backend/spec/operations_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::OperationsController, type: :controller do
-  def valid_input(operation, params)
+  def valid_input(operation, params, result)
     post operation, params: params
     expect(response).to have_http_status(200)
     json_response = JSON.parse(response.body)
@@ -18,9 +18,9 @@ RSpec.describe Api::V1::OperationsController, type: :controller do
     end
 
     if operation == :add
-      expect(json_response['result']).to eq(first.to_i + second.to_i)
+      expect(json_response['result']).to eq(result)
     elsif operation == :subtract
-      expect(json_response['result']).to eq(first.to_i - second.to_i)
+      expect(json_response['result']).to eq(result)
     end
   end
 
@@ -32,15 +32,21 @@ RSpec.describe Api::V1::OperationsController, type: :controller do
 
   describe 'POST #add' do
     it 'returns the sum of two numbers' do
-      valid_input(:add, { first: 10, second: 5 })
+      valid_input(:add, { first: 10, second: 5 }, 15)
     end
 
     it 'returns the sum of two numbers if one of them has a trailing space' do
-      valid_input(:add, { first: '10   ', second: 5 })
+      valid_input(:add, { first: '10   ', second: 5 }, 15)
     end
 
     it 'returns the sum of two numbers if one of them has a leading space' do
-      valid_input(:add, { first: 10, second: '   6' })
+      valid_input(:add, { first: 10, second: '   6' }, 16)
+    end
+
+    context 'decimal numbers' do
+      it 'returns the sum of two decimal numbers' do
+        valid_input(:add, {first: 0.1, second: 0.2}, 0.3)
+      end
     end
 
     context 'there is an invalid character in the input field' do
@@ -57,18 +63,18 @@ RSpec.describe Api::V1::OperationsController, type: :controller do
 
     context 'there is an empty input field' do
       it 'treats the value as 0 if the first field is empty' do
-        valid_input(:add, { second: 5 })
+        valid_input(:add, { second: 5 }, 5)
       end
 
       it 'treats the value as 0 if the second field is empty' do
-        valid_input(:add, { first: 8 })
+        valid_input(:add, { first: 8 }, 8)
       end
     end
   end
 
   describe 'POST #subtract' do
     it 'returns the difference of two numbers' do
-      valid_input(:subtract, { first: 10, second: 5 })
+      valid_input(:subtract, { first: 10, second: 5 }, 5)
     end
 
     context 'there is an invalid character in the input field' do
@@ -85,11 +91,11 @@ RSpec.describe Api::V1::OperationsController, type: :controller do
 
     context 'there is an empty input field' do
       it 'treats the value as 0 if the first field is empty' do
-        valid_input(:subtract, { second: 6 })
+        valid_input(:subtract, { second: 6 }, -6)
       end
 
       it 'treats the value as 0 if the second field is empty' do
-        valid_input(:subtract, { first: 9 })
+        valid_input(:subtract, { first: 9 }, 9)
       end
     end
   end
