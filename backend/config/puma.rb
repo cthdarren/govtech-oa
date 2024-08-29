c094d3f481dd2d6b9c151a5dbb064227c97c46c4
cthdarren <ctze.darren@gmail.com>
2024-08-29 11:41:49 +0200

    added letsencrypt cert for https

--- backend/config/puma.rb
+++ backend/config/puma.rb
@@ -16,6 +16,12 @@ if ENV["RAILS_ENV"] == "production"
   require "concurrent-ruby"
   worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { Concurrent.physical_processor_count })
   workers worker_count if worker_count > 1
+
+  ssl_bind '0.0.0.0', '443', {
+    key: "/etc/letsencrypt/live/govtech-oa-backend.darren-chan.com/privkey.pem",
+    cert: "/etc/letsencrypt/live/govtech-oa-backend.darren-chan.com/fullchain.pem",
+  verify_mode: 'none'
+}
 end
 
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
