373adb19bbe22035d9ce764ea02db09c8c1850eb
cthdarren <ctze.darren@gmail.com>
2024-08-29 16:18:24 +0200

    finally deployment works flawlessly

--- backend/config/puma.rb
+++ backend/config/puma.rb
@@ -16,12 +16,6 @@ if ENV['RAILS_ENV'] == 'production'
   require 'concurrent-ruby'
   worker_count = Integer(ENV.fetch('WEB_CONCURRENCY') { Concurrent.physical_processor_count })
   workers worker_count if worker_count > 1
-
-  ssl_bind '0.0.0.0', '3000', {
-    key: '/etc/cert/privkey.pem',
-    cert: '/etc/cert/fullchain.pem',
-    verify_mode: 'none'
-  }
 end
 
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
@@ -30,6 +24,12 @@ worker_timeout 3600 if ENV.fetch('RAILS_ENV', 'development') == 'development'
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 port ENV.fetch('PORT') { 3000 }
+
+ssl_bind '0.0.0.0', '3001', {
+  key: '/etc/letsencrypt/live/govtech-oa-backend.darren-chan.com/privkey.pem',
+  cert: '/etc/letsencrypt/live/govtech-oa-backend.darren-chan.com/fullchain.pem',
+  verify_mode: 'none'
+}
 
 # Specifies the `environment` that Puma will run in.
 environment ENV.fetch('RAILS_ENV') { 'development' }
